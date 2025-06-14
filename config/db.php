a1ab416e3be3a101d542d96162564b7e79adcf35
hamidayubdev <hamid.ayub@arbisoft.com>
2025-06-14 17:06:01 +0500

    added hero section with slider

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username = "root";
-$password = "";
+$password = "Abc_1234.";
 $database = "stayease";
 
 $conn = new mysqli($servername, $username, $password, $database);
